85b4232afbe076c4259452768991a28e3e54cb6b
trisha332 <mondaltrisha432@gmail.com>
2024-10-29 09:51:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    
+    <footer class="footer-distributed">
+        <div class="footer-left">
+            <h2>Soner <span>Hasel</span></h2>
+       
+        <p class="footer-links">
+            <a href="#">Home</a>
+            |
+            <a href="#">About</a>
+            |
+            <a href="#">Contact</a>
+            |
+            <a href="#">Blog</a>
+        </p>
+        <p class="footer-company-name">Copyright © 2021 <strong>Soner Hasel</strong> All rights reserved </p>
+    </div>
+    <div class="footer-center">
+        <div>
+            <i class="fa fa-phone"></i>
+            <p>+91 7501179463</p>
+        </div>
+        <div>
+            <i class="fa fa-envelope"></i>
+            <p><a href="#">sonerhasel78@gmail.com</a></p>
+        </div>
+    </div>
+        <div class="footer-right">
+            <p class="footer-company-about">
+               <span>About the company</span>
+               <strong>Soner Hasel</strong> is authentic Bengali Restaurant where you can find authentic Bengali food item.</p>
+            <div class="footer-icons">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                            
+
+           
+
+        </div>
+
+        
+    </div>
+        
+    </footer>
+</body>
+</html>
